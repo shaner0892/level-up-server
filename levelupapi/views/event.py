c07a82d9e2c698a0e4841c296d37ae279caaa21e
Hilary Shaner <hilaryshaner@gmail.com>
2022-05-03 16:01:31 -0500

    ch 7-9

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -1,10 +1,11 @@
 """View module for handling requests about game types"""
-from asyncio import events
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Event
+from levelupapi.models.game import Game
+from levelupapi.models.gamer import Gamer
 
 
 class EventView(ViewSet):
@@ -29,6 +30,37 @@ class EventView(ViewSet):
         """
         events = Event.objects.all()
         serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    # Inside the method, the first line of code is getting the game that
+    # is logged in. Since all of our postman or fetch requests have the
+    # user’s auth token in the headers, the request will get the user
+    # object based on that token. From there, we use the request.auth.user
+    # to get the Gamer object based on the user. Here’s the equivalent sql.
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        organizer = Gamer.objects.get(user=request.auth.user)
+        # Next, we retrieve the GameType object from the database. We do
+        # this to make sure the game type the user is trying to add the
+        # new game actually exists in the database. The data passed in
+        # from the client is held in the request.data dictionary.
+        # Whichever keys are used on the request.data must match what the
+        # client is passing to the server.
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            game = game,
+            description = request.data["description"],
+            date = request.data["date"],
+            time = request.data["time"],
+            organizer = organizer
+        )
+
+        serializer = EventSerializer(event)
         return Response(serializer.data)
     
 class EventSerializer(serializers.ModelSerializer):
--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from levelupapi.models import Game
+from levelupapi.models.game_type import GameType
+from levelupapi.models.gamer import Gamer
 
 
 class GameView(ViewSet):
@@ -29,9 +31,42 @@ class GameView(ViewSet):
         serializer = GameSerializer(games, many=True)
         return Response(serializer.data)
 
+    # Inside the method, the first line of code is getting the game that
+    # is logged in. Since all of our postman or fetch requests have the
+    # user’s auth token in the headers, the request will get the user
+    # object based on that token. From there, we use the request.auth.user
+    # to get the Gamer object based on the user. Here’s the equivalent sql.
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        # Next, we retrieve the GameType object from the database. We do
+        # this to make sure the game type the user is trying to add the
+        # new game actually exists in the database. The data passed in
+        # from the client is held in the request.data dictionary.
+        # Whichever keys are used on the request.data must match what the
+        # client is passing to the server.
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+
+        game = Game.objects.create(
+            title=request.data["title"],
+            maker=request.data["maker"],
+            number_of_players=request.data["number_of_players"],
+            skill_level=request.data["skill_level"],
+            gamer=gamer,
+            game_type=game_type
+        )
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+
+
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
     class Meta:
         model = Game
-        fields = ('id', 'game_type', 'title', 'maker', 'gamer', 'number_of_players', 'skill_level')
+        fields = ('id', 'game_type', 'title', 'maker',
+                  'gamer', 'number_of_players', 'skill_level')
